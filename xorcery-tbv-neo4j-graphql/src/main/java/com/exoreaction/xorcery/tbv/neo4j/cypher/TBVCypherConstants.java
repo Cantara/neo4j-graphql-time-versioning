fd46e321c71009be54ea8fa5b7906d89c41caba9
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2022-08-09 13:15:14 +0200

    Establish public constants for generic node labels

--- xorcery-tbv-neo4j-graphql/src/main/java/com/exoreaction/xorcery/tbv/neo4j/cypher/TBVCypherConstants.java
+++ xorcery-tbv-neo4j-graphql/src/main/java/com/exoreaction/xorcery/tbv/neo4j/cypher/TBVCypherConstants.java
@@ -3,4 +3,6 @@ package com.exoreaction.xorcery.tbv.neo4j.cypher;
 public class TBVCypherConstants {
 
     public static final String PARAMETER_IDENTIFIER_TIME_BASED_VERSION = "_tbv";
+    public static final String LABEL_RESOURCE_NODE = "RESOURCE";
+    public static final String LABEL_INSTANCE_NODE = "INSTANCE";
 }
