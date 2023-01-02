938cbf3177aabd161c96b799a8ff0df541eb44e2
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2023-01-02 12:07:29 +0100

    Removed unused constant

--- xorcery-tbv-neo4j-plugin/src/main/java/com/exoreaction/xorcery/tbv/neo4j/apoc/path/TBVConstants.java
+++ xorcery-tbv-neo4j-plugin/src/main/java/com/exoreaction/xorcery/tbv/neo4j/apoc/path/TBVConstants.java
@@ -1,15 +1,11 @@
 package com.exoreaction.xorcery.tbv.neo4j.apoc.path;
 
 import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
-
-import java.util.Collections;
 
 public class TBVConstants {
     static final RelationshipType RELATIONSHIP_TYPE_VERSION = RelationshipType.withName("VERSION");
     public static final Label LABEL_RESOURCE = Label.label("RESOURCE");
     public static final Label LABEL_INSTANCE = Label.label("INSTANCE");
     public static final Label LABEL_EMBEDDED = Label.label("EMBEDDED");
-    static final Iterable<Relationship> EMPTY = Collections.emptyList();
 }
